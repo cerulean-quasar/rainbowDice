7bc00c8beac7a1e3d24d4e2b46d2b8ed6b001e0e
Cerulean Quasar <cerulean.quasar@gmail.com>
2018-04-14 13:34:50 -0700

    cleanup

--- obj/local/arm64-v8a/objs/rainbowDice/home/katrina/AndroidStudioProjects/RainbowDice/jni/__/app/src/main/cpp/rainbowDice.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/arm64-v8a/objs/rainbowDice//home/katrina/AndroidStudioProjects/RainbowDice/jni/__/app/src/main/cpp//rainbowDice.o: \
-  /home/katrina/AndroidStudioProjects/RainbowDice/jni/../app/src/main/cpp//rainbowDice.cpp
--- obj/local/armeabi-v7a/objs/rainbowDice/home/katrina/AndroidStudioProjects/RainbowDice/jni/__/app/src/main/cpp/rainbowDice.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/armeabi-v7a/objs/rainbowDice//home/katrina/AndroidStudioProjects/RainbowDice/jni/__/app/src/main/cpp//rainbowDice.o: \
-  /home/katrina/AndroidStudioProjects/RainbowDice/jni/../app/src/main/cpp//rainbowDice.cpp
