b999ba1b85a949099af70e2a949519b4578999d5
Cerulean Quasar <cerulean.quasar@gmail.com>
2018-11-16 12:02:34 -0800

    Changed the application name of the Vulkan instance.

--- app/src/main/cpp/graphicsVulkan.cpp
+++ app/src/main/cpp/graphicsVulkan.cpp
@@ -90,7 +90,7 @@ namespace vulkan {
     void Instance::createInstance() {
         VkApplicationInfo appInfo = {};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "AmazingLabyrinth";
+        appInfo.pApplicationName = "RainbowDice";
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
